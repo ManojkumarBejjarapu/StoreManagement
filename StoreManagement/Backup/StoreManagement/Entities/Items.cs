ed18402ea141832699d8741700a88830a2387e3c
KonthamShiva <shiva.svec43@gmail.com>
2014-07-22 19:16:35 +0530

    Comment
    initial  work has completed we need to work in same solution

---
+++ StoreManagement/Backup/StoreManagement/Entities/Items.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using SqlConnector;
+using System.Data;
+using SuperMarket.ResponceClass;
+
+namespace SuperMarket.Classes
+{
+    public class Items
+    {
+        const string SUCCESS_MSG = "Successfully {0} a book";
+        const string Failure_MSG = "Unable to {0} a book";
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal CP { set; get; }
+        public decimal SP { set; get; }
+        public int StockAvailable { get; set; }
+
+        public Response Create()
+        {
+            SqlConnector.DBOperations objdb = new DBOperations(StoreManagement.Properties.Settings.Default.StoreManagement);
+            string stInsert = @"INSERT INTO [Items]([Name],[CP],[SP],[StockAvailable])
+             VALUES(@Name, @CP,@SP, @StockAvailable";
+            string[] strAddParameterName = new string[] { "Name", "CP", "SP" ,"StockAvailable"};
+            object[] objAddparametervalue = new object[] { this.Name, this.CP, this.SP,this.StockAvailable };
+            if (!objdb.QuerryExecutor(stInsert, strAddParameterName, objAddparametervalue))
+                return new Response(9999, string.Format(Failure_MSG, "insert"));
+
+            else
+
+                return new Response(5555, string.Format(SUCCESS_MSG, "inserted"));
+        }
+
+        public Response Delete()
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.payRoll);
+            string strDelete = "DELETE FROM [Items] WHERE Id=@Id";
+            string[] strAddParameterName = new string[] { "Id" };
+            object[] objAddparametervalue = new object[] { this.Id };
+            if (!objdb.ExecuteQuery(strDelete, strAddParameterName, objAddparametervalue))
+
+                return new Response(9999, string.Format(Failure_MSG, "delete"));
+            else
+
+                return new Response(5555, string.Format(SUCCESS_MSG, "deleted"));
+        }
+
+        public Response Update()
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.payRoll);
+            string strUpsate = @"UPDATE [Items]
+            SET [Name] = @Name
+            ,[CP] = @CP
+             ,[SP] = @SP
+            ,[StockAvailable] = @StockAvailable, int,>
+                WHERE Id=@Id ";
+            string[] strAddParameterName = new string[] { "Name", "CP", "SP", "Id" };
+            object[] objAddparametervalue = new object[] { this.Name, this.CP, this.SP, this.Id };
+            if (!objdb.ExecuteQuery(strUpsate, strAddParameterName, objAddparametervalue))
+                return new Response(9999, string.Format(Failure_MSG, "update"));
+            else
+                return new Response(5555, string.Format(SUCCESS_MSG, "updated"));
+        }
+
+        public static Items Get(int workerId)
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.payRoll);
+
+            Items objItems = new Items();
+            string strGetOneRecord = "SELECT * FROM [Items] where Id=@Id";
+            string[] strAddParameterName = new string[] { "Id" };
+            object[] objAddparametervalue = new object[] { objItems.Id };
+            System.Data.DataTable objtab = objdb.ExecuteGetAllQuery(strGetOneRecord, strAddParameterName, objAddparametervalue);
+            return objItems;
+        }
+
+        public static List<Items> GetAll()
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.payRoll);
+            List<Items> lstItems = new List<Items>();
+            Items objItems = new Items();
+
+            string strGetOneRecord = "SELECT * FROM [Items]";
+            string[] strAddParameterName = new string[] { };
+            object[] objAddparametervalue = new object[] { };
+            DataTable objtab = new DataTable();
+            objtab = objdb.ExecuteGetAllQuery(strGetOneRecord, strAddParameterName, objAddparametervalue);
+
+            for (int i = 0; i < objtab.Rows.Count; i++)
+            {
+                int intId;
+                int intStockAvailable;
+                decimal decCP;
+                decimal decSP;
+                objItems = new Items();
+                int.TryParse(objtab.Rows[i]["Id"].ToString(), out intId);
+                objItems.Id = intId;
+                objItems.Name = objtab.Rows[i]["Name"] != null ? objtab.Rows[i]["Name"].ToString() : string.Empty;
+                decimal.TryParse(objtab.Rows[i]["CP"].ToString(), out decCP);
+                objItems.CP = decCP;
+                decimal.TryParse(objtab.Rows[i]["SP"].ToString(), out decSP);
+                objItems.SP = decSP;
+                lstItems.Add(objItems);
+            }
+            return lstItems;
+
+        }
+
+
+    }
+}
---
+++ StoreManagement/Backup/StoreManagement/StoreManagementService.asmx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using SuperMarket.ResponceClass;
+
+namespace StoreManagement
+{
+    /// <summary>
+    /// Summary description for StoreManagementService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class StoreManagementService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public Response InsertProducts()
+        {
+            
+            return new Response(10, "Error");
+        }
+       
+    }
+}
